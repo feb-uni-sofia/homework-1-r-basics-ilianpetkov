9dc0f371741b45b770c551e09166b18f60ad0d12
ilian petkov <burningabyss@abv.bg>
2018-04-01 00:10:45 +0300

    Done, I hope :D

--- Problem_1.R
+++ Problem_1.R
@@ -1,1 +1,18 @@
-
+# a)
+x <- c(4,1,1,4)
+# b)
+y <- c(1,4)
+# c)
+Difference <- x - y # it subtracted the numbers elementwise but as a set of two :  first 4,1 - 1,4 giving us 3,-3 and then 1,4 - 1,4 giving us 0,0
+# d)
+s <- c(x, y)
+# e)
+rep(s,10) 
+length(rep(s,10))
+# f)
+rep(s, each = 3)
+# g) 
+seq(7, 21, by=1)
+7:21
+# h)
+length(7:21)
